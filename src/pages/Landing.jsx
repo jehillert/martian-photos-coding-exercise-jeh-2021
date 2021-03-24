e07a31ba808032b07826ec3ed25564d50ca5d141
John Hillert <john.hillert@gmail.com>
2021-03-23 23:40:09 -0500

    intial commit

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Cracked from 'app/components/Animation/Cracked';
+
+import './Landing.css';
+
+const Landing = () => <Cracked>martian-photos-coding-exercise-jeh-2021</Cracked>;
+
+export default Landing;
