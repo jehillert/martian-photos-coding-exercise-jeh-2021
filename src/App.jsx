13ea78299c8b077fa6e610a0ef5056b27b87a66f
John Hillert <john.hillert@gmail.com>
2021-03-24 00:44:42 -0500

    remove unnecessary template files

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
 import 'app/theme';
 
 import Loading from 'app/components/Loading';
-import AppRoutes from 'app/routes/AppRoutes';
 
-const App = () => (
-  <Suspense fallback={<Loading />}>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <AppRoutes />
-    </BrowserRouter>
-  </Suspense>
-);
+const App = () => <Suspense fallback={<Loading />}> wait </Suspense>;
 
 export default App;
--- src/pages/Landing.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Cracked from 'app/components/Animation/Cracked';
-
-import './Landing.css';
-
-const Landing = () => <Cracked>martian-photos-coding-exercise-jeh-2021</Cracked>;
-
-export default Landing;
